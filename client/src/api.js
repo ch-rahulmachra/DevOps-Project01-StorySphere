cb994eacb8808498a65302420232125256750202
Rahul Machra <ch.rahulmachra@gmail.com>
2025-11-28 21:47:59 +0530

    new modification

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-export const api = axios.create({ baseURL: 'http://localhost:80/api' });
-
+export const api = axios.create({ baseURL: process.env.REACT_APP_SERVER_URL });
+// 'http://localhost:80/api'
 export const authHeader = () => {
   const t = localStorage.getItem('token');
   return t ? { Authorization: `Bearer ${t}` } : {};
