602bbe55587c73014fb3ba4c0220d22196a33a06
Rahul Machra <ch.rahulmachra@gmail.com>
2025-12-16 22:42:51 +0530

    changed backend also

--- server/src/index.js
+++ server/src/index.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 sequelize.authenticate()
   .then(() => {
-    console.log("✅ Connected to DB");
+    console.log("✅ Connected to DB!!!");
     return sequelize.sync({ alter: true });
   })
   .then(() => {
